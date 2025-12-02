02965af8021d259357aa4d2681e46be57c8b5a3c
Darius Kassi <kassidarius@gmail.com>
2025-12-02 18:51:57 +0000

    chore(school): add tw-animate-css animation library dependency

--- apps/school/src/components/layout/app-layout.tsx
+++ apps/school/src/components/layout/app-layout.tsx
@@ -1,25 +1,21 @@
-import { useState } from 'react'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import { Header } from './header'
-import { MobileSidebar } from './mobile-sidebar'
-import { Sidebar } from './sidebar'
+import { AppSidebar } from './sidebar'
 
 interface AppLayoutProps {
   children: React.ReactNode
 }
 
 export function AppLayout({ children }: AppLayoutProps) {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
-
   return (
-    <div className="relative flex min-h-screen flex-col">
-      <Header onMenuClick={() => setIsMobileMenuOpen(true)} />
-      <div className="flex flex-1">
-        <Sidebar />
-        <MobileSidebar isOpen={isMobileMenuOpen} onClose={() => setIsMobileMenuOpen(false)} />
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <Header />
         <main className="flex-1 overflow-auto">
-          <div className="container py-6">{children}</div>
+          <div className="container p-6">{children}</div>
         </main>
-      </div>
-    </div>
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
